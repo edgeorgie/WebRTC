f09855b37a487e95d1e4616b5c0e1f08b9197ae7
Edwin Jorge <ed.jorge1122@gmail.com>
2022-10-21 16:17:38 -0500

    EDIT: app and index file structure for routing and importing modules

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const app = express()
 
+const server = require('http').createServer(app)
+const cors = require('cors')
+
+const routes = require('./routes')
+
+app.use(cors())
+app.use('/', routes)
+
+module.exports = {
+  app,
+  server
+}
