81796e400dfec1731a2d67e375e27b649eaa01f0
Edwin Jorge <ed.jorge1122@gmail.com>
2022-10-21 16:18:21 -0500

    EDIT: socket.io instances and configuration

--- services/socket.js
+++ services/socket.js
@@ -1,1 +1,31 @@
+const { server } = require('../app')
 
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.emit('me', socket.id)
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('Connection finished')
+  })
+
+  socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('calluser', {
+      signal: signalData,
+      from,
+      name
+    })
+  })
+
+  socket.on('answercall', (data) => {
+    io.to(data.to).emit('callaccepted', data.signal)
+  })
+})
+
+module.exports = io
